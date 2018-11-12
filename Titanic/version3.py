66bca09fd14fdb9c1fea001f8da8aeefdc055dbf
Paul <paul.amory@student.isae-supaero.fr>
2018-11-12 19:55:45 +0100

    Added : Neural Networks, 'deep' neural networks, xgboost

--- Titanic/version3.py
+++ Titanic/version3.py
@@ -1,7 +1,18 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Activation
+import keras.losses as losses
+import keras.optimizers as optimizers
+import keras.metrics as metrics
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import mean_absolute_error, accuracy_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn import svm
@@ -28,7 +39,9 @@ class PreprocessData:
         data_raw.drop(to_drop, axis='columns', inplace=True)
         le = preprocessing.LabelEncoder()
         data_raw['Sex'] = le.fit_transform(data_raw['Sex'])
-        data_raw['Embarked'] = le.fit_transform(data_raw['Embarked'])
+        one_hot = pd.get_dummies(data_raw['Embarked'])
+        data_raw = data_raw.drop(['Embarked'], axis=1)
+        data_raw = data_raw.join(one_hot)
         data_raw.drop(columns='Family')
         return data_raw
 
@@ -61,21 +74,12 @@ class PreprocessData:
         dataset.drop(['SibSp', 'Parch'], axis='columns', inplace=True)
         return dataset
 
-path_train = 'train.csv'
-path_test = 'test.csv'
-prep = PreprocessData()
-data_train = prep.preprocess_data(path_train)
-to_be_trained, data_test_n = train_test_split(data_train, test_size=0.2, random_state=40, shuffle=True)
-X_train = to_be_trained.iloc[:, 1:to_be_trained.shape[1]]
-y_train = to_be_trained.iloc[:, 0]
-X_test = data_test_n.iloc[:, 1:data_test_n.shape[1]]
-y_test = data_test_n.iloc[:, 0]
-
-mySVC = svm.SVC(kernel='linear', C=10)
-mySVC.fit(X_train, y_train)
-print(mySVC.score(X_test, y_test))
-############# ENTRAINEMENT #####################
-
+# ############# ENTRAINEMENT #####################
+#to_be_trained, data_test_n = train_test_split(data_train, test_size=0.2, random_state=40, shuffle=True)
+# X_train = to_be_trained.iloc[:, 1:to_be_trained.shape[1]]
+# y_train = to_be_trained.iloc[:, 0]
+# X_test = data_test_n.iloc[:, 1:data_test_n.shape[1]]
+# y_test = data_test_n.iloc[:, 0]
 # tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-1, 1e-2],
 #                      'C': [1, 10, 100]},
 #                     {'kernel': ['linear'], 'C': [1, 10]}]
@@ -108,12 +112,72 @@ print(mySVC.score(X_test, y_test))
 #     print(classification_report(y_true, y_pred))
 #     print()
 
+
+path_train = 'train.csv'
+path_test = 'test.csv'
+prep = PreprocessData()
+data_train = prep.preprocess_data(path_train)
 data_test = prep.preprocess_data(path_test)
+to_be_trained, data_test_n = train_test_split(data_train, test_size=0.2, random_state=40, shuffle=True)
+X_train = to_be_trained.iloc[:, 1:to_be_trained.shape[1]]
+y_train = to_be_trained.iloc[:, 0]
+X_test = data_test_n.iloc[:, 1:data_test_n.shape[1]]
+y_test = data_test_n.iloc[:, 0]
 
-ypred = mySVC.predict(data_test)
-my_submission = pd.DataFrame(data={'PassengerId':data_test.index, 'Survived':ypred})
 
-print(my_submission['Survived'].value_counts())
+################ XGBoost ##########################
+#
+# my_model = XGBClassifier(n_estimators=1000, learning_rate=0.05)
+# my_model.fit(X_train, y_train, early_stopping_rounds=5,
+#              eval_set=[(X_test, y_test)], verbose=True)
+# predictions = my_model.predict(X_test)
+# print("Mean Absolute Error : " + str(mean_absolute_error(predictions, y_test)))
+# print("Accuracy score : " + str(accuracy_score(predictions, y_test)))
+#
+# ############### SVC ##########################
+# #
+# mySVC = svm.SVC(kernel='linear', C=10.0)
+# mySVC.fit(X_train, y_train)
+# predictions = mySVC.predict(X_test)
+# print("Mean Absolute Error : " + str(mean_absolute_error(predictions, y_test)))
+# print("Accuracy score : " + str(accuracy_score(predictions, y_test)))
 
-#Export Results to CSV
-my_submission.to_csv('submission.csv', index=False)
+# ############### Neural Networks ##########################
+# myNN = MLPClassifier(hidden_layer_sizes=(10))
+# myNN.fit(X_train, y_train)
+# predictions = myNN.predict(data_test)
+# print("Accuracy score : " + str(myNN.score(X_test,y_test)))
+# ############### Deep Networks ##########################
+# model_nn = Sequential()
+# model_nn.add(Dense(units=5, input_dim=X_train.shape[1]))
+# model_nn.add(Activation('relu'))
+# model_nn.add(Dense(units=5))
+# model_nn.add(Activation('relu'))
+# model_nn.add(Dense(units=2))
+# model_nn.add(Activation('softmax'))
+# y_train_bin = to_categorical(y_train)
+# y_test_bin = to_categorical(y_test)
+# model_nn.compile(loss=losses.categorical_crossentropy,
+#               optimizer=optimizers.SGD(lr=0.001, decay=0., nesterov=False),
+#               metrics=[metrics.categorical_accuracy])
+# history =model_nn.fit(X_train, y_train_bin, validation_data=(X_test,y_test_bin), epochs=300, batch_size=64)
+# plt.figure(figsize=(20,10))
+#
+# for i, (name, values) in enumerate(sorted(history.history.items())):
+#     plt.subplot(1, len(history.history.items()), i+1)
+#     plt.plot(values)
+#     plt.title(name)
+#
+# plt.show()
+# ############### Best results ########################## 258-160
+#  my_model = XGBClassifier(n_estimators=1000, learning_rate=0.05)
+# my_model.fit(X_train, y_train, early_stopping_rounds=5,
+#              eval_set=[(X_test, y_test)], verbose=True)
+# predictions = my_model.predict(data_test)
+
+# my_submission = pd.DataFrame(data={'PassengerId':data_test.index, 'Survived':predictions})
+#
+# print(my_submission['Survived'].value_counts())
+#
+# #Export Results to CSV
+# my_submission.to_csv('submission.csv', index=False)
